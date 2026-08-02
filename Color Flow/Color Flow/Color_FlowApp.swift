594895f57faff5606e96df7f1f4bff73cbf9b7bc
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:27:12 -0400

    Add Color Flow/Color Flow/Color_FlowApp.swift

---
+++ Color Flow/Color Flow/Color_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_FlowApp.swift
+//  Color Flow
+//
+//  Created by Elliot Williams on 2025-06-09.
+//
+
+import SwiftUI
+
+@main
+struct Color_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MusicLibraryCoverFlow()
+        }
+    }
+}
